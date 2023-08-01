aa14c4cc8e52a3ff65a5918fb132e76c0848a256
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-07-31 21:06:01 -0600

    Deleting some files and adding new

---
+++ src/GiftApp.jsx
@@ -0,0 +1,12 @@
+
+
+export const GiftApp = () => {
+  return (
+    <>
+    
+        <h1>Gif App React</h1>
+    
+    
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import { GiftApp } from './GiftApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GiftApp />
   </React.StrictMode>,
 )
