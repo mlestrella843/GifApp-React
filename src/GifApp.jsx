085e072909c2687dae097aeb40cfca43f85da846
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-01 18:38:20 -0600

    Instaling React-Bootstrap

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,20 +1,49 @@
 
+  import { useState } from "react";
+  import Button from 'react-bootstrap/Button';
+  import Form from 'react-bootstrap/Form';
+  import InputGroup from 'react-bootstrap/InputGroup';
 
-export const GifApp = () => {
-  return (
-    <>
-        {/* Title */}
-        <h1>Gif App React</h1>
+  export const GifApp = () => {
 
-        
-        {/* Input */}
+      const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+      //console.log(categories);
+      //Funcion que agrega una categoria
+      const onAddCategory = () => {
+          //console.log('valor');
+          //los tres puntos delante significa que hara un espejo de todo lo que contiene el arreglo categories
+          //setCategories([ ...categories, 'valor']);
+          //Los nuevos valores se alnmacenan en categories, setCategories es solo la funcion para alterar el estado
+          //de categories
+          //Si el nuevo valor se quiere insertar al inicio se coloca asi
+          setCategories([ 'valor', ...categories ]);
+          console.log(categories);
+      }
+      return (
+        <>
+          <div className="container mt-1"> 
+            {/* Title */}
+            <h1>Gif App React</h1>
+         
+            {/* Input */}
+            <input className="mb-3 b-5" />
 
+            {/* Listado fr Gifs */}
 
+            <br/>
+            <Button variant="primary mt-2" onClick={ onAddCategory }>Add Category</Button>
+                {/* Gif Item */}
 
-        {/* Listado fr Gifs */}
-            {/* Gif Item */}
-        
-    
-    </>
-  )
+                <ol>
+                {
+                    categories.map( category => {
+                       return <li key={ category }>{ category } </li> 
+                    })
+                }
+                </ol>
+                    
+          </div>         
+        </>
+      )
+
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import { GifApp } from './GifApp';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GifApp />
