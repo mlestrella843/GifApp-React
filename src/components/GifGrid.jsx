f4093ad436557cee6d9c71d79158d9dc882c1ac9
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-04 10:21:41 -0600

    Displaying data in DOM

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,19 +1,33 @@
 
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGif } from "../helpers/getGif";
 
 
 export const GifGrid = ( { category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGif( category );
+        setImages(newImages);
+    }
+
     useEffect( () => {
-        getGif(category);
+        getImages();
     }, [])
 
     return (
         <>
             <h3>{ category}</h3>  
+            <ol>          
+                {
+                    images.map( ({ id, title }) => (
+                         <li key={ id }>{ title }</li>
+                    ))
+                }
+                
+            </ol>
         </>
   )
-
 }
