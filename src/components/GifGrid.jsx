3d1c58ec3dc92e6216a10d75724c131d04bbc0e6
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-09 11:41:25 -0600

    Adding is Loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -4,12 +4,16 @@ import { useFetchGifs } from "../hooks/useFetchGifs";
 
 
 export const GifGrid = ( { category }) => {
-    
+
     const {  images, isLoading } = useFetchGifs( category );
 
     return (
         <>
             <h3>{ category}</h3>  
+            {
+                isLoading && ( <h2>is Loading...</h2>)
+            }
+
             <div className="card-grid" >          
                 {
                     images.map( ( image ) => (
