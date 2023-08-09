8bbf2f4c20ad6b06a606fdc6e21b5bedfecfecb5
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-09 14:20:29 -0600

    Adding a Barrel file for components

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,14 +1,12 @@
 
   import { useState } from "react";
-  import { AddCategory } from "./components/AddCategory";
-  import { GifGrid } from "./components/GifGrid";
+  import { AddCategory, GifGrid } from "./components";
  
   export const GifApp = () => {
 
       const [ categories, setCategories ] = useState(['Iron Man']);
      
       const onAddCategory = ( newCategory ) => {
-          //console.log( newCategory );
           if( categories.includes(newCategory) ) return; 
           setCategories( [ newCategory, ...categories ] );      
       }
