2f6e7e1c4ec92c92bbe8adb7b79143b4c4ecebe1
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-08-13 10:00:51 -0600

    Adding dynamic styles

---
+++ src/BienvenueApp.jsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import './estilos.css';
+
+ export const BienvenueApp = () => {
+  
+    return (
+    <div>
+        <div className='titleInitial'>
+               <h3> WELCOME TO GIF APP </h3>
+        </div>
+        <div>
+              <button>
+                 <span>Click to Start!</span>
+               </button>
+        </div>
+    </div>
+  )
+
+}
+
+
+
+
+
+  
+  
+ 
+  
+
+ 
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './estilos.css';
 //import './style.css';
-import { GifApp } from './GifApp';
+// import { GifApp } from './GifApp';
+import { BienvenueApp } from './BienvenueApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GifApp />
+    {/* <GifApp /> */}
+
+    <BienvenueApp />
+
  </React.StrictMode>
 );
